4ed690eefb0a0b995c4d02fa4a0945d4009340fb
gb-123-git <64229388+gb-123-git@users.noreply.github.com>
2021-09-22 15:42:10 +0530

    Update cdecode.c

--- cores/esp32/libb64/cdecode.c
+++ cores/esp32/libb64/cdecode.c
@@ -40,6 +40,7 @@ static int base64_decode_block_signed(const int8_t* code_in, const int length_in
           fragment = (int8_t)base64_decode_value_signed(*codechar++);
         } while (fragment < 0);
         *plainchar    = (fragment & 0x03f) << 2;
+        break;
       case step_b:
         do {
           if (codechar == code_in+length_in){
@@ -51,6 +52,7 @@ static int base64_decode_block_signed(const int8_t* code_in, const int length_in
         } while (fragment < 0);
         *plainchar++ |= (fragment & 0x030) >> 4;
         *plainchar    = (fragment & 0x00f) << 4;
+        break;
       case step_c:
         do {
           if (codechar == code_in+length_in){
@@ -62,6 +64,7 @@ static int base64_decode_block_signed(const int8_t* code_in, const int length_in
         } while (fragment < 0);
         *plainchar++ |= (fragment & 0x03c) >> 2;
         *plainchar    = (fragment & 0x003) << 6;
+        break;
       case step_d:
         do {
           if (codechar == code_in+length_in){
@@ -72,6 +75,7 @@ static int base64_decode_block_signed(const int8_t* code_in, const int length_in
           fragment = (int8_t)base64_decode_value_signed(*codechar++);
         } while (fragment < 0);
         *plainchar++   |= (fragment & 0x03f);
+        break;
     }
   }
   /* control should not reach here */
